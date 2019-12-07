cc0d01a1bb5912dc8b93781d2a19d950de18a6d5
Stefan <stefan.egger@posteo.de>
2019-12-07 11:47:22 +0100

    working

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 db.create_all()
 
 @app.route("/", methods=["GET"])
-def index ():
-    email_adress = request.cookies.get("email")
+def index():
+    email_address = request.cookies.get("email")
 
-    if email_adress:
-        user = db.query(User).filter_by(email=email_adress).first()
+    if email_address:
+        user = db.query(User).filter_by(email=email_address).first()
     else:
         user = None
     return render_template("index.html", user=user)
 
 @app.route("/login", methods=["POST"])
 def login():
-    name = request.form.get("user_name")
-    email = request.form.get ("user_email")
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
 
-    secret_number = random.randint (1, 10)
+    secret_number = random.randint(1, 10)
     user = db.query(User).filter_by(email=email).first()
 
     if not user:
         user = User(name=name, email=email, secret_number=secret_number)
-
         db.add(user)
         db.commit()
+
     response = make_response(redirect(url_for("index")))
     response.set_cookie("email", email)
 
@@ -37,16 +37,16 @@ def login():
 @app.route("/result", methods=["POST"])
 def result():
     guess = int(request.form.get("guess"))
-    email_adress = request.cookies.get("email")
+    email_address = request.cookies.get("email")
 
-    user = db.query(User).filter_by(email=email_adress).first()
+    user = db.query(User).filter_by(email=email_address).first()
 
     if guess == user.secret_number:
         message = "Richtig! Die geheime Zahl ist {0}".format(str(guess))
-        new_secret = random.randint (1, 30)
+        new_secret = random.randint (1, 10)
         user.secret_number = new_secret
 
-        db.add(User)
+        db.add(user)
         db.commit()
 
     elif guess > user.secret_number:
